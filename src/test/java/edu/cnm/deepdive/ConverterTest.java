ca30729618624924220de99c913f40f26630e611
Nick Bennett <nick@nickbenn.com>
2022-01-25 14:31:43 -0700

    Conversion test working.

---
+++ src/test/java/edu/cnm/deepdive/ConverterTest.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConverterTest {
+
+  @Test
+  void convertC2F() {
+    assertEquals(212, Converter.convertC2F(100));
+    assertEquals(32, Converter.convertC2F(0));
+    assertEquals(-40, Converter.convertC2F(-40));
+  }
+
+}
